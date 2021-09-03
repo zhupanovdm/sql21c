80a6b84d9bf129723b59412f4760ab7c4031cc48
zhupanovdm <zhupanov.dmitry@gmail.com>
2021-09-03 11:37:25 +0300

    Minor fixes

--- src/main/java/org/zhupanovdm/sql21c/transform/ParserUtils.java
+++ src/main/java/org/zhupanovdm/sql21c/transform/ParserUtils.java
@@ -4,9 +4,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ParserUtils {
+    private static final Pattern TABLE_NAME_PATTERN = Pattern.compile("\\s*\\[\\s*'?(.*?)'?\\s*]\\s*");
+    private static final Pattern INCORRECT_PARAM_PATTERN = Pattern.compile("@\\d[_\\d\\w]*+");
 
     public static String replaceIncorrectParams(String input) {
-        Matcher matcher = Pattern.compile("@\\d[_\\d\\w]*+").matcher(input);
+        Matcher matcher = INCORRECT_PARAM_PATTERN.matcher(input);
         StringBuilder sb = new StringBuilder();
         while (matcher.find()) {
             String group = matcher.group();
@@ -18,8 +20,9 @@ public class ParserUtils {
     }
 
     public static String toEntityName(String input) {
-        if (input.charAt(0) == '[') {
-            return input.replace("[", "").replace("]", "");
+        Matcher matcher = TABLE_NAME_PATTERN.matcher(input);
+        if (matcher.find()) {
+            return matcher.group(1);
         }
         return input;
     }

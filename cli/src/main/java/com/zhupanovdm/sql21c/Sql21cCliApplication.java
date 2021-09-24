bb06ad48b752ff7aaf6df952cb6cb5393ac604ea
zhupanovdm <zhupanov.dmitry@gmail.com>
2021-09-24 11:11:10 +0300

    CLI app proto added

---
+++ cli/src/main/java/com/zhupanovdm/sql21c/Sql21cCliApplication.java
@@ -0,0 +1,34 @@
+package com.zhupanovdm.sql21c;
+
+public class Sql21cCliApplication {
+    public static void main(String[] args) {
+//        Options options = new Options();
+//
+//        Option input = new Option("i", "input", true, "input file path");
+//        input.setRequired(true);
+//        options.addOption(input);
+//
+//        Option output = new Option("o", "output", true, "output file");
+//        output.setRequired(true);
+//        options.addOption(output);
+//
+//        CommandLineParser parser = new DefaultParser();
+//        HelpFormatter formatter = new HelpFormatter();
+//        CommandLine cmd = null;//not a good practice, it serves it purpose
+//
+//        try {
+//            cmd = parser.parse(options, args);
+//        } catch (ParseException e) {
+//            System.out.println(e.getMessage());
+//            formatter.printHelp("utility-name", options);
+//
+//            System.exit(1);
+//        }
+//
+//        String inputFilePath = cmd.getOptionValue("input");
+//        String outputFilePath = cmd.getOptionValue("output");
+//
+//        System.out.println(inputFilePath);
+//        System.out.println(outputFilePath);
+    }
+}
--- gui/src/main/java/com/zhupanovdm/sql21c/Sql21cGuiApplication.java
+++ gui/src/main/java/com/zhupanovdm/sql21c/Sql21cGuiApplication.java
@@ -5,14 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Sql21cApplication extends Application {
+public class Sql21cGuiApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(Sql21cApplication.class.getResource("main-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Sql21cGuiApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("SQL statement to 1C transformer");
         stage.setScene(scene);
